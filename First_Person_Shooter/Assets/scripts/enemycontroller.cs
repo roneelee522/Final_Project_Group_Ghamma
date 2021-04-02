8b3c4a309e6360779b52925c93aefbbcf3e7f247
roneelee522 <roneelee522@hotmail.com>
2021-04-02 11:24:06 -0400

    add enemy explode

---
+++ First_Person_Shooter/Assets/scripts/enemycontroller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemycontroller : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent nav;
+    public float range;
+    public Transform playerDistance;
+    public GameObject enemyExplosion;
+    public float explosionRange;
+  
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(playerDistance.position, transform.position) <= range)
+        {
+            nav.SetDestination(target.position);
+        }
+
+        if (Vector3.Distance(playerDistance.position, transform.position) <= explosionRange)
+        {
+            Explode();
+        }
+
+    }
+
+    void Explode()
+    {
+        Instantiate(enemyExplosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
+

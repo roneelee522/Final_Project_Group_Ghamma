216d1d3585aa72fd742ccfe516920d15bc31c09b
jthig613 <67336532+jthig613@users.noreply.github.com>
2021-04-08 12:10:42 -0400

    health

--- First_Person_Shooter/Assets/scripts/bullettime.cs
+++ First_Person_Shooter/Assets/scripts/bullettime.cs
@@ -6,6 +6,7 @@ public class bullettime : MonoBehaviour
 {
     public Rigidbody rb;
     public int speed;
+    public int damage;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +20,24 @@ public class bullettime : MonoBehaviour
         Destroy(gameObject, 2.0f);
         rb.AddForce(transform.forward * speed);
         rb.AddForce(transform.up * 2f, ForceMode.Impulse);
+
+            if (collider.CompareTag("Enemy"))
+            {
+                Debug.Log("Enemy hit");
+                DestroyProjectile();
+                col.GetComponent<EnemyController>().TakeDamage(damage);
+            }
+    }
+
+    void OnCollisionEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player"){
+        Debug.Log("Hi there");
+        }
+
+        if(col.CompareTag("Player"))
+        {
+            col.GetComponent<PlayerHealth>().TakeDamage(damage);
+        }
     }
 }

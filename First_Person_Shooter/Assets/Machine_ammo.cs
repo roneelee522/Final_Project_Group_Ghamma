938ff7cb6d7354832ff18c5cea6e17f4c5223665
Kevin Perez <kperez49@knights.ecf.edu>
2021-04-22 01:21:33 -0400

    Machin_ammo_bug_fix

--- First_Person_Shooter/Assets/Machine_ammo.cs
+++ First_Person_Shooter/Assets/Machine_ammo.cs
@@ -8,10 +8,15 @@ public class Machine_ammo : MonoBehaviour
      public GameObject ammoBox;
     public Gun script;
     // Start is called before the first frame update
+
+    void Update()
+    {
+        script = GameObject.Find("Player/Main Camera/Weapon_Holder/Machine Gun(Clone)").GetComponent<Gun>();
+    }
  void OnTriggerEnter(Collider other)
  {
+     
      ammoBox.SetActive(false);
      script.pistolinvAmmo += 70;
  }
-
 }
